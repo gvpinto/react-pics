200facfc9b4266d5da4c2221101edac050b5c765
Glenn Pinto <gvpinto@gmail.com>
2021-07-12 20:15:43 -0400

    91. solving context issues (this)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 
 export class SearchBar extends Component {
-  state = { term: "" };
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form className='ui form' onSubmit={this.onFormSubmit}>
           <div className='field'>
             <label htmlFor='imageSearch'>Image Search</label>
             <input
               type='text'
               value={this.state.term}
-              onChange={(e) =>
-                this.setState({ term: e.target.value.toUpperCase() })
-              }
+              onChange={(e) => this.setState({ term: e.target.value })}
               name='imageSearch'
             />
           </div>
